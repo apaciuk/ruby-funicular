d6643bce1314e3ffe37cd28d1b542292253b5963
Paul Anthony McGowan <apacicloud@gmail.com>
2023-07-02 13:50:20 +0100

    Views and controller responses

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -5,30 +5,45 @@ class BaseController
          @request = request
    end 
 
-    def index 
-        build_response <<-HTML
-        <html>
-            <head><title>Ruby Funicular</title></head>
-            <body>
-                <h1>Welcome to Ruby Funicular</h1>
-                <p>It's a fun ride!</p>
-            </body>
-        </html>
-        HTML
-    end
+   def index 
+        build_response render_template
+   end
 
    private  
 
    def build_response(body, status: 200)
-    [status, { "Content-Type" => "text/html" }, [body]]
-   end
+        [status, { "Content-Type" => "text/html" }, [body]]
+   end 
 
-   def redirect_to(uri)
+   def render_template(name = params[:action])
+        templates_dir = self.class.name.downcase.sub("controller", "") 
+        template_file = File.join(templates_dir, "#{name}.html.erb")
+
+        file_path = template_file_path_for(template_file) 
+
+        if File.exist?(file_path)
+            puts "Rendering template: #{template_file}"
+            render_erb_file(file_path) 
+        else
+            "ERROR: no available template file #{template_file}"
+        end
+    end 
+
+  
+    def template_file_path_for(file_name)
+        File.expand_path(File.join("../../views", file_name), __FILE__)
+    end 
+
+    def render_erb_file(file_path)
+        File.read(file_path)
+    end
+
+    def redirect_to(uri)
          build_response("", 302, "Location" => uri)
-   end 
+    end 
 
-   def params
-         @request.params
-   end 
+    def params
+        @request.params
+    end 
 
 end
